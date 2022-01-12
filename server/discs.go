62dcd7f4b774bb72d64f21c623e913a0d756187d
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2022-01-11 19:17:46 -0500

    add error logging and wrapped discs response

--- server/discs.go
+++ server/discs.go
@@ -2,7 +2,11 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"discgolfapi.com/m/models"
+	"github.com/rs/zerolog/log"
 )
 
 // Discs ... Get all discs
@@ -11,16 +15,19 @@ import (
 // @Success 200 {array} models.Disc
 // @Router /discs [get]
 func Discs(w http.ResponseWriter, r *http.Request) {
-	resp, err := Db.GetDiscs()
+	discs, err := Db.GetDiscs()
 	if err != nil {
+		log.Error().Msg(fmt.Sprintf("Error when getting discs from database: %s", err.Error()))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 
+	resp := models.DiscsResponse{Discs: discs}
 	jsonResponse, err := json.Marshal(resp)
 	if err != nil {
+		log.Error().Msg(fmt.Sprintf("Error when marshalling json response: %s", err.Error()))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
