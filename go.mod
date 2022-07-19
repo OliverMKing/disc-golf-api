28cd75b3c926b7ccac2f7f7303bab83445125ea8
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2022-07-18 21:30:55 -0400

    remove old code and add openapi spec

--- go.mod
+++ go.mod
@@ -2,19 +2,4 @@ module discgolfapi.com/m
 
 go 1.16
 
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/jackc/pgx/v4 v4.14.1 // indirect
-	github.com/lib/pq v1.10.4 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/rs/zerolog v1.26.1
-	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
-	github.com/swaggo/http-swagger v1.1.2
-	github.com/swaggo/swag v1.7.8
-	golang.org/x/crypto v0.0.0-20220128200615-198e4374d7ed // indirect
-	golang.org/x/net v0.0.0-20220105145211-5b0dc2dfae98 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/tools v0.1.8 // indirect
-	gorm.io/driver/postgres v1.2.3
-	gorm.io/gorm v1.22.5
-)
+require github.com/rs/zerolog v1.26.1
--- main.go
+++ main.go
@@ -1,46 +1,10 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"time"
-
-	. "discgolfapi.com/m/server"
 	"github.com/rs/zerolog/log"
 )
 
-// @title Disc Golf API
-// @version 0.0.0
-// @description   Open-source Disc Golf API of discs and their attributes. You can view the source code [here](https://github.com/OliverMKing/disc-golf-api). Please submit any issues or feature requests [here](https://github.com/OliverMKing/disc-golf-api/issues).
-
-// @license.name  MIT License
-// @license.url   https://github.com/OliverMKing/disc-golf-api/blob/main/LICENSE
-
-// @host localhost:8080
-// @BasePath /api
 func main() {
-	srv := GetServer()
 
-	// start server
-	go func() {
-		log.Info().Msg("Starting http server")
-		if err := srv.ListenAndServe(); err != nil {
-			log.Fatal().Msg(err.Error())
-		}
-	}()
-
-	// handle graceful shutdown
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c // block until signal received
-
-	// wait for existing connections to finish
-	wait := time.Second * 15
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-
-	log.Info().Msg("Shutting down")
-	srv.Shutdown(ctx)
-	os.Exit(0)
+	log.Info().Msg("Hello World!")
 }
